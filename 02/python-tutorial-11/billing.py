90d778a39904054813aa1c0ee0555574f62edc41
raslam07 <120336895+raslam07@users.noreply.github.com>
2024-09-23 22:20:51 -0400

    Add files via upload

---
+++ 02/python-tutorial-11/billing.py
@@ -0,0 +1,20 @@
+def calculate_tax(price, tax):
+    return price * tax
+
+
+def print_billing_doc():
+    tax_rate = 0.1
+    products = [{'name': 'Book',  'price': 30},
+                {'name': 'Pen', 'price': 5}]
+
+    # print billing header
+    print(f'Name\tPrice\tTax')
+
+    # print the billing item
+    for product in products:
+        tax = calculate_tax(product['price'], tax_rate)
+        print(f"{product['name']}\t{product['price']}\t{tax}")
+
+
+if __name__ == '__main__':
+    print_billing_doc()
